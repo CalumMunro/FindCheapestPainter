6b29fabf17ccf05ec6244cfb8955974d394bd851
Calum Munro <calum.munro@amorgroup.local>
2017-07-14 14:46:10 +0100

    Add project files.

---
+++ FindCheapestPainter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindCheapestPainter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
