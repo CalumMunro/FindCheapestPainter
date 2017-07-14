1fcedb314b6a168e37e7636a888f27723009382b
Calum Munro <camunro03@gmail.com>
2017-07-14 16:28:41 +0100

    SAVEPOINT
    SAVEPOINT

--- FindCheapestPainter/Program.cs
+++ FindCheapestPainter/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks.Dataflow;
 
 namespace FindCheapestPainter
 {
@@ -13,6 +14,25 @@ namespace FindCheapestPainter
                 painters
                     .Where(painter => painter.isAvailable)
                     .WithMinimum(painter => painter.EstimateCompensation(sqMeters));
+        }
+
+        private static IPainter FindFastestPainter(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return
+                painters
+                    .Where(painter => painter.isAvailable)
+                    .WithMinimum(painter => painter.EstimateTimeToPaint(sqMeters));
+        }
+
+        private static void WorkTogether(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            TimeSpan time =
+                TimeSpan.FromHours(
+                    1 /
+                    painters
+                        .Where(painter => painter.isAvailable)
+                        .Select(painter => 1 / painter.EstimateTimeToPaint(sqMeters).TotalHours)
+                        .Sum());
         }
 
         static void Main(string[] args)
