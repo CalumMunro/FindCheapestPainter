fddfbd5d92d983863bf7aaf8f088b9c699d65fa1
Calum Munro <camunro03@gmail.com>
2017-07-14 16:03:24 +0100

    Create iterating example and helper method.

--- FindCheapestPainter/Program.cs
+++ FindCheapestPainter/Program.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿using FindCheapestPainter.Painter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FindCheapestPainter
 {
     class Program
     {
+        private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return
+                painters
+                    .Where(painter => painter.isAvailable)
+                    .WithMinimum(painter => painter.EstimateCompensation(sqMeters));
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
