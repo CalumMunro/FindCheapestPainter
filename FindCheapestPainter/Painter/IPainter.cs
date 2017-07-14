4ffb5b647636cd5092654e636339aa5211382c99
Calum Munro <camunro03@gmail.com>
2017-07-14 14:51:21 +0100

    created IPainter.

---
+++ FindCheapestPainter/Painter/IPainter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindCheapestPainter.Painter
+{
+    public interface IPainter
+    {
+        bool isAvailable { get; }
+        TimeSpan EstimateTimeToPaint(double sqMeters);
+        double EstimateCompensation(double sqMeters);
+    }
+}
